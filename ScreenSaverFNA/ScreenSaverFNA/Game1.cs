2151d60f6b4e5c506d792d73d0e33cf7c0a16665
medvedevaviktoria <medvedevaviktoria176@gmail.com>
2025-11-02 16:44:12 +0300

    Создание проекта и добавление изображений

---
+++ ScreenSaverFNA/ScreenSaverFNA/Game1.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ScreenSaverFNA
+{
+    public class Game1 : Game
+    {
+        Texture2D snowflakeTexture;
+        Texture2D backgroundTexture;
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();            
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            snowflakeTexture = Content.Load<Texture2D>("snowFlake");
+            backgroundTexture = Content.Load<Texture2D>("winterBackground");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(backgroundTexture, new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height), Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ ScreenSaverFNA/ScreenSaverFNA/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new ScreenSaverFNA.Game1();
+game.Run();
